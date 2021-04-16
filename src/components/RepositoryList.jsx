b4fb2393ad9f1ebbe97c7183bc5d04699f739170
brenombastos <brenodead@gmail.com>
2021-04-16 18:56:29 -0300

    Inicia implementação do tsx e config typescript

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,29 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
-const repositoryObject = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    //O use efect dois parametros o primeiro é qual função ele deve executar, 
+    //o segundo e quando deve executar
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);//DEPENDENCIAS Sempre que repositories mudar a função dentro de {} é executada, SE [] TIVER VAZIO A FUNÇÃO SÓ EXECUTA 1 VEZ
+
+    console.log(repositories);
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-
-                <RepositoryItem repository={repositoryObject} />
-                <RepositoryItem repository={repositoryObject} />
-                <RepositoryItem repository={repositoryObject} />
-
-
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     )
