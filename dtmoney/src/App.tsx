a0f38854048a9eff7b32dac6099694b5c058968e
brenombastos <brenodead@gmail.com>
2021-06-03 13:38:18 -0300

    Commit

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,13 @@
+import { GlogalStyle } from "./styles/glogal";
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1 >Hello World</h1>
+
+      <GlogalStyle />
+    </div>
+  );
+}
+
